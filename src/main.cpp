9bdad8f8ea4e3881043035d29e837db464c13ea8
liketofight69 <tyler.sand12@gmail.com>
2017-04-16 22:41:54 -0700

    created png handling function
    For ease of loading pngs

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,51 @@
 
 #include "App.h"
 #include <cstdio>
+#include "main.h"
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+ void main1::disp_PNG()
+   {
+
+
+    SDL_Surface * pngSurf = IMG_Load("sprite.PNG");
+    SDL_Texture * pngTex = SDL_CreateTextureFromSurface(App::getRenderer(), pngSurf);
+    SDL_FreeSurface(pngSurf);
+
+    SDL_Texture * texTarget = SDL_CreateTexture(App::getRenderer(), SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET,SCREEN_WIDTH,SCREEN_HEIGHT);
+
+
+    SDL_SetRenderTarget(App::getRenderer(),texTarget);
+    SDL_RenderClear(App::getRenderer());
+    SDL_RenderCopy(App::getRenderer(), pngTex, NULL, NULL);
+
+    SDL_SetRenderTarget(App::getRenderer(), NULL);
+
+    SDL_RenderClear(App::getRenderer());
+    SDL_RenderCopyEx(App::getRenderer(),texTarget, NULL, NULL, 0, NULL, SDL_FLIP_VERTICAL);
+    SDL_RenderPresent(App::getRenderer());
+   }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 int main(int argc, char *args[])
 {
+    //IMG_INIT(IMG_INIT_PNG);
+
     App app1;
 
     if(!app1.init())
@@ -17,13 +57,20 @@ int main(int argc, char *args[])
         printf("Error initializing... bye!\n");
         return 0;
     }
-    app1.start();
-    app1.run();
+    //app1.start();
+    //app1.run();
 
-    SDL_Surface * image = IMG_Load("sprite.png");
+
+    //SDL_Surface * image = IMG_Load("sprite.png");
+    //SDL_Texture * texture = SDL_CreateTextureFromSurface(getRenderer,image);
+
+
+
+    app1.run();
 
 
 
 
     return 0;
 }
+
---
+++ src/main.h
@@ -0,0 +1,12 @@
+#include "Game.h"
+
+class main1
+{
+   //~main();
+
+    void disp_PNG();
+
+
+
+
+};
