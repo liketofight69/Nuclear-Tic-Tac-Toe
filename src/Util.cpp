d7f2757aedd6a3088293eaee11a08c0434cf96b4
liketofight69 <tyler.sand12@gmail.com>
2017-04-16 18:27:38 -0700

    error handler
    Added an image error handler.

--- src/Util.cpp
+++ src/Util.cpp
@@ -6,12 +6,25 @@
 #include "Util.h"
 #include "App.h"
 #include <cmath>
+#include <stdio.h>
 
 // render cross on board field (row, col)
 void Util::drawCross(int row, int col)
 {
     int fieldW = App::SCREEN_WIDTH/3;
     int fieldH = App::SCREEN_HEIGHT/3;
+
+    IMG_Init(IMG_INIT_PNG);
+    SDL_Surface * image = IMG_Load("sprite.PNG");
+
+    if(!image) {
+        printf("IMG_LOAD: %s\n", IMG_GetError());
+    }
+
+
+    SDL_Texture * texture = SDL_CreateTextureFromSurface(App::getRenderer(),image);
+
+
 
     // top left to bottom right
     SDL_SetRenderDrawColor(App::getRenderer(),//SDL_Render_DrawLine
@@ -30,6 +43,11 @@ void Util::drawCross(int row, int col)
         col*fieldW+fieldW, row*fieldH+fieldH);
 
 
+
+
+
+
+    SDL_FreeSurface(image);
 
 
 
@@ -62,5 +80,6 @@ void Util::drawCircle(int row, int col)
         endX = r * cos(angle) + centerX;
         endY = r * sin(angle) + centerY;
         SDL_RenderDrawLine(App::getRenderer(), startX, startY, endX, endY);
+
     }
 }
